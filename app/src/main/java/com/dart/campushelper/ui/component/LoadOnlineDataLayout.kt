cc20fa8513756e3a37a0b24620fc1dcf48022f53
Founchoo <founchoo@outlook.com>
2023-12-14 09:18:11 +0800

    修复 ModalBottomSheet 中下拉刷新时内容高度突变消失无法交互的 issue

--- app/src/main/java/com/dart/campushelper/ui/component/LoadOnlineDataLayout.kt
+++ app/src/main/java/com/dart/campushelper/ui/component/LoadOnlineDataLayout.kt
@@ -1,8 +1,7 @@
 package com.dart.campushelper.ui.component
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
@@ -18,7 +17,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.dart.campushelper.data.Result
 import com.dart.campushelper.data.Status
 import io.github.fornewid.placeholder.foundation.PlaceholderHighlight
@@ -86,11 +84,7 @@ fun <T> LoadOnlineDataLayout(
     ) {
         if (dataSource.status == Status.LOADING || isRefreshing) {
             // Waiting for data to load from network
-            Box(
-                Modifier
-                    .height(400.dp)
-                    .fillMaxWidth()
-            )
+            Box(Modifier.fillMaxSize())
         } else if (dataSource.data == null) {
             // If fail to load data from network, show a placeholder
             FailToLoadPlaceholder(::onRefresh)
